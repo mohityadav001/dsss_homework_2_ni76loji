e763ca345189be4c23549596730a618ac51df127
mohityadav001 <mohit.y20singh@gmail.com>
2024-11-08 16:59:47 +0100

    updating new files

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from setuptools import setup
 
 setup(
    name='dsss_homework_2',
-#    version='1.0',
+   version='1.0',
    description='Exercise 02 module',
    author='Mohit Yadav',
    author_email='mohit.yadav@fau.de',
    packages=['math_quiz'],  #same as name
    install_requires=['numpy', 'pandas'], #external packages as dependencies
-   scripts=[
-            'scripts/cool',
-            'scripts/skype',
-           ]
 )
